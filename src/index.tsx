e51ca54eb0a2f1d937e918fb7862c5266452b37b
puwon2a62 <puwon2a62@gmail.com>
2021-09-08 09:05:47 +0900

    standardJS / prettier setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-render(<div>Hello ReactJS</div>, document.getElementById(
-    "root"
-) as HTMLElement)
+render(
+  <div>Hello ReactJS</div>,
+  document.getElementById('root') as HTMLElement
+)
